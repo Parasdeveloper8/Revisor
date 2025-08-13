21387db5571bbf3dbd50f4b8abff6b028606f1ad
Parasdeveloper8 <parasprajapat8th@gmail.com>
2025-08-13 18:12:48 +0530

    Loader component created

---
+++ Client/Revisor-client/src/app/ui/loader/loader.tsx
@@ -0,0 +1,17 @@
+import "./loader.css";
+
+const Loader = () =>{
+    return(
+        <>
+         <div className="book-loader">
+      <div className="book">
+        <div className="page"></div>
+        <div className="page"></div>
+        <div className="page"></div>
+      </div>
+       <span>Loading..</span>
+    </div>
+        </>
+    )
+}
+export default Loader 
