8fd23cb9c943a9a60eacb441c8413e377a328336
Parasdeveloper8 <parasprajapat8th@gmail.com>
2025-09-20 23:23:55 +0530

    Added styles to result page

--- Client/Revisor-client/src/app/ui/result/result.tsx
+++ Client/Revisor-client/src/app/ui/result/result.tsx
@@ -1,41 +1,62 @@
-import { useLocation, useNavigate } from 'react-router-dom';
-import Trophy from '../../../assets/animations/trophy.mp4';
+import { useLocation, useNavigate } from "react-router-dom";
+import Trophy from "../../../assets/animations/trophy.mp4";
 import "./result.css";
-const ResultPage = () =>{
-     const location = useLocation();
-     const navigate = useNavigate();
-     const { result } = location.state || {};
-      type QuizResult = {
-         Marks : number;
-         TotalMarks : number;
-       }
-       const isValidResults = result && typeof (result as QuizResult).Marks === "number" && typeof (result as QuizResult).TotalMarks === "number";
-       if(!isValidResults){
-          return (
-        <div className="error-boundary">
+import { useEffect } from "react";
+
+const ResultPage = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const { result } = location.state || {};
+
+  type QuizResult = {
+    Marks: number;
+    TotalMarks: number;
+  };
+
+  const isValidResults =
+    result &&
+    typeof (result as QuizResult).Marks === "number" &&
+    typeof (result as QuizResult).TotalMarks === "number";
+
+     useEffect(() => {
+    document.body.classList.add("result-body");
+    return () => {
+      document.body.classList.remove("result-body");
+    };
+  }, [location.pathname]);
+
+  if (!isValidResults) {
+    return (
+      <div className="result-error">
         <h2>It's not you, it's us.</h2>
         <p>Something went wrong with loading the Results</p>
         <button onClick={() => navigate("/")}>Go Back Home</button>
       </div>
     );
-       }
-    return (
-        <>
-        <button id="homeBtn" onClick={() => navigate("/")}>
+  }
+  return (
+    <>
+      <button className="result-homeBtn" onClick={() => navigate("/")}>
         Home
       </button>
-        <div id="resultDiv">
-         <video 
-                src={Trophy} 
-                autoPlay 
-                loop 
-                muted 
-                playsInline
-                style={{ width: "300px", height: "auto", display: "block", margin: "0 auto" }}
-            />
-            <p>Your score is <b>{(result as QuizResult).Marks}</b></p>
-            </div>
-        </>
-    )
-}
-export default ResultPage
+      <div className="result-container">
+        <video
+          src={Trophy}
+          autoPlay
+          loop
+          muted
+          playsInline
+          className="result-trophy"
+        />
+        <p className="result-score">
+          Your score is{" "}
+          <b>
+            {(result as QuizResult).Marks}/{(result as QuizResult).TotalMarks}
+          </b>
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default ResultPage;
